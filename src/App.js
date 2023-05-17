f20ffb310e6ec8de10fb6b9201a680a972034feb
Marcos Aarón Castro Rivas <maracastror@gmail.com>
2023-05-17 14:22:22 -0600

    add: components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
 import freeCodeCampLogo from './images/freecodecamp-logo.png';
 
 function App() {
+  
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
       <div className='freeCodeCamp-logo-contenedor'>
@@ -11,7 +22,16 @@ function App() {
           alt='Logo de freeCodeCamp'/>
       </div>
       <div className='contenedor-principal'>
-
+        <Contador 
+          numClics={5}/>
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={ true }
+          manejarClic={ manejarClic } />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={ false }
+          manejarClic={ reiniciarContador } />
       </div>
     </div>
   );
